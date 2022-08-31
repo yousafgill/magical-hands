d0ae559c2051f292e7b28b10c304930c920c854a
Yousaf Gill <yousafgill@gmail.com>
2022-08-31 08:14:50 +0500

    magical hands commit

--- javascript/module1/variables.js
+++ javascript/module1/variables.js
@@ -7,6 +7,32 @@
     pear: 'green'
   };
 
-  console.log(bag); // red
+  let bag2 = {
+    [fruit.toUpperCase()]: 'RED',
+    pear: 'GREEN'
+  };
 
+
+  //create function to load data from web API
+  function loadData(url) {
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', url);
+      xhr.onload = () => resolve(xhr.responseText);
+      xhr.onerror = () => reject(xhr.statusText);
+      xhr.send();
+    }).then(data => JSON.parse(data));
+  }
+  //load data from web API
+  loadData('https://jsonplaceholder.typicode.com/photos')
+    .then(data => {
+      console.log(data);
+    }).catch(err => {
+      console.log(err);
+    }).finally(() => {
+      console.log('Finally');
+    });
+
+  console.log(bag); // red
+  console.log(bag2); // RED
 })();
